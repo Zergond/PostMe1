6edfbbd96bdf120cf8b038c7a56c9f86a918d6d5
Zergond <pavlokulesha.job@gmail.com>
2019-03-28 17:59:36 +0200

    init

---
+++ precache-manifest.4bcb6145f558d68db4fa2f28660fc602.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3238cb9980649ef19085",
+    "url": "/static/js/runtime~main.133f5243.js"
+  },
+  {
+    "revision": "d4db3ed674edd4046c34",
+    "url": "/static/js/main.05e9c7b8.chunk.js"
+  },
+  {
+    "revision": "931daf1234d0707c3403",
+    "url": "/static/js/3.ad0acc0c.chunk.js"
+  },
+  {
+    "revision": "456353d265b96b4eeeac",
+    "url": "/static/js/2.b7975e3f.chunk.js"
+  },
+  {
+    "revision": "c79b82234c749ac6295d492286e1430f",
+    "url": "/index.html"
+  }
+];
